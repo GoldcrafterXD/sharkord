0ad7c647d67dba096f036583f0874d414ed14660
Tobias Herbitschek <tobias.herbitschek@gmail.com>
2026-02-22 17:18:41 +0100

    feat(#337): Add toggle for showing user banners in voice chat

--- apps/client/src/features/server/slice.ts
+++ apps/client/src/features/server/slice.ts
@@ -61,6 +61,7 @@ export interface IServerState {
   };
   pluginCommands: TCommandsMapByPlugin;
   hideNonVideoParticipants: boolean;
+  showUserBannersInVoice : boolean;
   pluginComponents: TPluginComponentsMap;
 }
 
@@ -98,6 +99,9 @@ const initialState: IServerState = {
   pluginCommands: {},
   hideNonVideoParticipants: getLocalStorageItemBool(
     LocalStorageKey.HIDE_NON_VIDEO_PARTICIPANTS
+  ),
+  showUserBannersInVoice: getLocalStorageItemBool(
+    LocalStorageKey.VOICE_CHAT_SHOW_USER_BANNERS
   ),
   pluginComponents: {}
 };
@@ -721,6 +725,9 @@ export const serverSlice = createSlice({
     },
     setHideNonVideoParticipants: (state, action: PayloadAction<boolean>) => {
       state.hideNonVideoParticipants = action.payload;
+    },
+    setShowUserBannersInVoice: (state, action: PayloadAction<boolean>) => {
+      state.showUserBannersInVoice = action.payload;
     },
     addExternalStreamToChannel: (
       state,
